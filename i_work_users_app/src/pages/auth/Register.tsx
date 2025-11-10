8c0b2aecaaa5dc967a05e36448b97647e7b8cadc
Chandan Tiwari <Tiwarichandan936@gmail.com>
2025-11-10 19:30:58 +0530

    layout changes

--- i_work_users_app/src/pages/auth/Register.tsx
+++ i_work_users_app/src/pages/auth/Register.tsx
@@ -177,7 +177,7 @@ export function Register({ userType: propUserType, onBack }) {
                 <div className="flex items-center gap-3">
                   <label
                     htmlFor="aadhaarCard"
-                    className="flex items-center justify-center gap-2 px-4 py-2 bg-slate-700/40 border border-slate-600 rounded-lg text-gray-300 cursor-pointer hover:bg-slate-700 transition-all"
+                    className="flex items-center justify-center gap-2 px-4 sm:w-full py-2 bg-slate-700/40 border border-slate-600 rounded-lg text-gray-300 cursor-pointer hover:bg-slate-700 transition-all"
                   >
                     <Upload className="w-5 h-5" />
                     <span className="sm:text-lg text-sm">Upload Image</span>
